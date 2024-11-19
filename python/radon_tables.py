d6782f1f58b7ebda0e964c3ff471636586b404be
Mikko Partio <mikko.partio@fmi.fi>
2024-11-19 08:21:05 +0200

    Add new columns for views if radon version >= 20241119

--- python/radon_tables.py
+++ python/radon_tables.py
@@ -1028,7 +1028,155 @@ def DropTables(options):
             conn.commit()
 
 
+def CreateView20241119(options, element, class_id):
+    if class_id == 1:
+        query = """
+CREATE OR REPLACE VIEW public.%s_v AS
+SELECT
+		a.producer_id,
+		f.name AS producer_name,
+		a.analysis_time,
+		a.geometry_id,
+		g.name AS geometry_name,
+		a.param_id,
+		p.name AS param_name,
+		a.level_id,
+		l.name AS level_name,
+		a.level_value,
+		a.level_value2,
+		a.forecast_period,
+		a.forecast_period + a.analysis_time AS forecast_time,
+		a.file_location,
+		a.file_server,
+		a.forecast_type_id,
+		t.name AS forecast_type_name,
+		a.forecast_type_value,
+        a.aggregation_id,
+        ag.name AS aggregation_name,
+        a.aggregation_period,
+        a.processing_type_id,
+        pt.name AS processing_type_name,
+        a.processing_type_value,
+        a.processing_type_value2,
+		a.file_format_id,
+		ff.name AS file_format_name,
+		a.file_protocol_id,
+		fp.name AS file_protocol_name,
+		a.message_no,
+		a.byte_offset,
+		a.byte_length,
+		a.last_updater,
+		a.last_updated
+FROM
+		%s.%s a,
+		fmi_producer f,
+		level l,
+		param p,
+		geom g,
+		forecast_type t,
+		file_format ff,
+		file_protocol fp,
+        aggregation ag,
+        processing_type pt
+WHERE
+		a.producer_id = f.id
+		AND
+		a.level_id = l.id
+		AND
+		a.param_id = p.id
+		AND
+		a.geometry_id = g.id
+		AND
+		ff.id = a.file_format_id
+		AND
+		fp.id = a.file_protocol_id
+		AND
+		a.forecast_type_id = t.id
+        AND
+        ag.id = a.aggregation_id
+        AND
+        pt.id = a.processing_type_id
+""" % (
+            element["table_name"],
+            element["schema_name"],
+            element["table_name"],
+        )
+    elif class_id == 3:
+        query = """
+CREATE OR REPLACE VIEW public.%s_v AS
+SELECT
+		a.producer_id,
+		f.name AS producer_name,
+		a.analysis_time,
+		a.station_id,
+		st_x(s.position) AS longitude,
+		st_y(s.position) AS latitude,
+		a.param_id,
+		p.name AS param_name,
+		a.level_id,
+		l.name AS level_name,
+		a.level_value,
+		a.level_value2,
+		a.forecast_period,
+		a.forecast_period + a.analysis_time AS forecast_time,
+		a.forecast_type_id,
+		t.name AS forecast_type_name,
+		a.forecast_type_value,
+        a.aggregation_id,
+        ag.name AS aggregation_name,
+        a.aggregation_period,
+        a.processing_type_id,
+        pt.name AS processing_type_name,
+        a.processing_type_value,
+        a.processing_type_value2,
+		a.value,
+		a.last_updater,
+		a.last_updated
+FROM
+		%s.%s a,
+		fmi_producer f,
+		level l,
+		param p,
+		forecast_type t,
+		station s,
+        aggregation ag,
+        processing_type pt
+WHERE
+		a.producer_id = f.id
+		AND
+		a.level_id = l.id
+		AND
+		a.param_id = p.id
+		AND
+		a.forecast_type_id = t.id
+		AND
+		s.id = a.station_id
+        AND
+        ag.id = a.aggregation_id
+        AND
+        pt.id = a.processing_type_id
+		""" % (
+            element["table_name"],
+            element["schema_name"],
+            element["table_name"],
+        )
+
+    if options["show_sql"]:
+        print(query)
+
+    if not options["dry_run"]:
+        cur.execute(query)
+        query = "GRANT SELECT ON public.%s_v TO public" % (element["table_name"])
+        cur.execute(query)
+
+
 def CreateViews(options, element, class_id):
+
+    radon_version = get_radon_version()
+
+    if radon_version >= 20241119:
+        CreateView20241119(options, element, class_id)
+        return
 
     # Create a view on top of table
 
@@ -1416,9 +1564,13 @@ def CreateTables(options, element, date):
         conn.commit()
 
 
-def get_radon_version(cur):
+def get_radon_version():
     cur.execute("SELECT radon_version_f()")
-    return int(cur.fetchone()[0])
+
+    try:
+        return int(cur.fetchone()[0])
+    except Exception as e:
+        raise ValueError(f"Could not determine radon version: {e}")
 
 
 if __name__ == "__main__":
